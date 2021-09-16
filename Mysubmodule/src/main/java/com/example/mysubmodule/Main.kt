29d4e85f94e64dad441971dc95f65ff7a3a27058
zulqarnain <m.zulqarnain@purevpn.com>
2021-09-16 13:57:13 +0500

    added methods

--- Mysubmodule/src/main/java/com/example/mysubmodule/Main.kt
+++ Mysubmodule/src/main/java/com/example/mysubmodule/Main.kt
@@ -4,4 +4,6 @@ object Main {
 
     val name1: String = "Test module"
     fun getName(): String = name1
+    fun getFileName(): String = "file1"
+    fun getExtensionName(): String = "extension"
 }
