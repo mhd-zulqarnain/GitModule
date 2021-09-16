372b3f5f9434cac37d89653632a8bf8a563cad62
zulqarnain <m.zulqarnain@purevpn.com>
2021-09-16 14:04:51 +0500

    update message

--- Mysubmodule/src/main/java/com/example/mysubmodule/Main.kt
+++ Mysubmodule/src/main/java/com/example/mysubmodule/Main.kt
@@ -5,5 +5,5 @@ object Main {
     val name1: String = "Test module"
     fun getName(): String = name1
     fun getFileName(): String = "file1"
-    fun getExtensionName(): String = "extension"
+    fun getExtensionName(): String = "extension .text"
 }
